5bef19e3b3872c72560c2658bb3eec8950815865
sbussetti <steve.bussetti@gmail.com>
2021-07-18 13:59:43 -0400

    1626631183

--- bin/popcorn-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJ=$HOME/src/popcorn-devops
-ANSI=$PROJ/ansible
-ENV=${2-development}
-
-. /usr/local/bin/virtualenvwrapper.sh
-workon devops
-$PROJ/bin/sshagent.sh $HOME/.ssh/popcorn-sys.pem ansible-playbook -i $ANSI/inventory/$1 -l $ENV $ANSI/filmbot.yml
